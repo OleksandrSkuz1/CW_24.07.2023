20c936645c799fd9b17eaf62b419ec7913ba1bc7
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-24 19:29:07 +0300

    add

---
+++ main.py
@@ -0,0 +1,26 @@
+# def pib(name, secondName, thirdName):
+#     return f"{secondName.title()} {name[0].upper()}. {thirdName[0].upper()}."
+#     print(pib("John", "Doe", "Patrick"))
+
+# def sum(number):
+#     number += 1
+#     if number <= 10:
+#         print(number)
+#         sum(number)
+#     else:
+#         return number
+
+
+
+# def fib(first_number, second_number, end_number):
+#     while second_number <= end_number:
+#         next_number = first_number + second_number
+#         first_number = second_number
+#         second_number = next_number
+#         if second_number <= end_number:
+#             print(next_number)
+# if __name__ == '__main__':
+#     fib(1, 2, 100)
+
+
+
